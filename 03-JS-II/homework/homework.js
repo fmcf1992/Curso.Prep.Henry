348b59e8edc9c91e105ba33f9b044a133ddbb910
michael <michaelcastrofalen@hotmail.com>
2021-12-13 14:33:09 -0500

    modificada 3.21

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -190,6 +190,10 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor){
+    return true;
+  }
+  return false;
 
 }
 
