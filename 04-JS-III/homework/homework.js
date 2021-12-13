e379d84aa40395d88ec2f9a7549cf30a49d270ee
michael <michaelcastrofalen@hotmail.com>
2021-12-13 15:18:15 -0500

    modificada 4.1

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -37,7 +37,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  array=array.push(elemento);
+  array.push(elemento);
   return array;
 
 }
