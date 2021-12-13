6a5c2500d8db5f119ebefbbd5a92b0241f1a5d73
michael <michaelcastrofalen@hotmail.com>
2021-12-13 11:53:17 -0500

    modificada 2.8

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,7 +73,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(length(str1)===length(str2))
+  if(str1.length===str2.length)
   {
     return true;
   }
@@ -179,15 +179,16 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero>0){
-    return "Es positivo";
-  }
-  if(numero<0){
-    return "Es negativo";
-  } 
+  
   if(numero===0){
     return "false";
-  } 
+  }
+  if(numero!==0){
+    if(numero>0){
+      return "Es positivo";
+    }
+      return "Es negativo"
+  }
  
 }
 
