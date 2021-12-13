f0a33f2f095e947b0573ea9b2da1764b0b53a6d3
michael <michaelcastrofalen@hotmail.com>
2021-12-13 14:47:28 -0500

    modificada 3.220

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -151,6 +151,9 @@ function operadoresLogicos(num1, num2, num3) {
         num3=num3+1;
         return num3;
       }
+      else{
+        return false;
+      }
     }
   }else{
     if(num1===0 || num2===0 || num3===0){
