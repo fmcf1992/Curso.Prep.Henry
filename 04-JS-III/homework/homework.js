edabc1d720c923ffdaecf2722a2d2d7c49c6ac5a
michael <michaelcastrofalen@hotmail.com>
2021-12-13 23:10:26 -0500

    modificada 4.3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -123,12 +123,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var cant=0;
+  for(let i=0; i<arreglo.length; i++){
+    if(arreglo[i]>18){
+      cant++;
+    }
+  }
+  return cant;
 
 }
 
@@ -138,6 +151,16 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  switch(numeroDeDia){
+    case 1: return "Es fin de semana";
+    case 2: return "Es fin de semana";
+    case 3: return "Es dia Laboral";
+    case 4: return "Es dia Laboral";
+    case 5: return "Es dia Laboral";
+    case 6: return "Es dia Laboral";
+    case 7: return "Es dia Laboral";
+    default: return "No es un dia valido";
+  }
   
 } 
 
@@ -146,6 +169,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var arrayn=[];
+  arrayn=Array.from(n);
+  if(arrayn[0]==9){
+    return true;
+  }
+  return false;
   
 }
 
@@ -154,6 +183,17 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var x=arreglo[0];
+  var contador=0;
+  for(let i=0;i<arreglo.length;i++){
+    if(x===arreglo[i]){
+      contador++;
+    }
+  }
+  if(contador===arreglo.length){
+    return true;
+  }
+  return false;
   
 } 
 
@@ -163,13 +203,22 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+
 }
 
 
 function mayorACien(array) {
-  //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
+  //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array 
+  //sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var newA=[];
+  for(let i=0;i<array.length;i++){
+    if(array[i]>=100){
+      newA.push(array[i]);
+    }  
+  }
+  return newA;
 }
 
 
@@ -181,6 +230,16 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var newA=[];
+  var suma;
+  for(let i=0;i<10;i++){
+    suma=suma+2;
+    newA.push(suma);
+    if(suma===i+1){
+      break;
+    }
+  }
+  return "Se interrumpió la ejecución";
 }
 
 
